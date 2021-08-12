e4ab1f4bc895d184dea4b9efb918ea9ce5eec75f
israeloriente1 <israeloriente1@hotmail.com>
2021-08-12 14:58:52 -0300

    IMC Update
    Atualização nas descrição dos riscos em forma de lista

--- atividades/06 - imc/imc.js
+++ atividades/06 - imc/imc.js
@@ -2,37 +2,40 @@ function calcular() {
     altura = Number(document.getElementById("altura").value);
     peso = Number(document.getElementById("peso").value);
     resultado = document.getElementById("resultado");
+    riscosResult = document.getElementById('riscos');
 
     imc = peso / (altura * altura);
     imc = imc.toFixed(2);
 
+    // A variável (riscos) irá receber Strings dentro da tag <li>, para que assim que a variável for usado dentro de uma tag <ul> ou <ol> irá retonar as String em forma de lista.
+
     if (imc >= 16 && imc < 17) {
         status = "Muito abaixo do peso";
-        riscos = "Queda de cabelo, infertilidade e ausência menstrual.";
+        riscos = ["<li>Queda de cabelo</li>", "<li>Infertilidade</li>", "<li>Ausência menstrual</li>"];
 
     }else if (imc >= 17 && imc < 18.5) {
         status = "Abaixo do peso";
-        riscos = "Fadiga, Stress e ansiedade";
+        riscos = ["<li>Fadiga</li>", "<li>Stress</li>", "<li>Ansiedade</li>"];
 
     }else if (imc >= 18.5 && imc < 25) {
         status = "Peso Normal";
-        riscos = "Menos risco de doenças cardíacas e vasculares";
+        riscos = ["<li>Menos risco de doenças cardíacas</li>", "<li>Vasculares</li>"];
 
     }else if (imc >= 25 && imc < 30) {
         status = "Acima do peso";
-        riscos = "Fadiga, má circulação e varizes";
+        riscos = ["<li>Fadiga</li>", "<li>Má circulação</li>", "<li>Varizes</li>"];
 
     }else if (imc >= 30 && imc < 35) {
         status = "Obesidade Grau 1";
-        riscos = "Diabetes, angina, infarto e aterosclerose";
+        riscos = ["<li>Diabetes</li>", "<li>Angina</li>", "<li>Infarto</li>", "<li>Aterosclerose</li>"];
 
     }else if (imc >= 35 && imc <= 40) {
         status = "Obesisdade Grau 2";
-        riscos = "Apneia do sono e falta de ar";
+        riscos = ["<li>Apneia do sono</li>", "<li>Falta de ar</li>"];
 
     }else if (imc > 40) {
         status = "Obesisdade Grau 3";
-        riscos = "Refluxo, dificuldade para se mover, escaras, diabetes, infarto e AVC";
+        riscos = ["<li>Refluxo</li>", "<li>Dificuldade para se mover</li>", "<li>Escaras</li>", "<li>Diabetes</li>", "<li>Infarto</li>", "<li>AVC</li>"];
 
     }else {
         alert("Error: Algo deu errado, por favor tente novamente. ");
@@ -42,5 +45,10 @@ function calcular() {
     resultado.innerHTML = `
         <h2>IMC: ${imc} </h2>
         <p><strong>Status</strong>: ${status} </p>
-        <p><strong>Riscos</strong>: ${riscos} </p>`;
+        <p><strong>Riscos</strong> </p>`;
+        
+    riscosResult.innerHTML = `
+        <ul>
+            ${riscos}
+        </ul>`;    
 }
---
+++ atividades/teste.js
@@ -0,0 +1,7 @@
+var valores = [42, 345, 437, 3, 65, 45, 225, 23, 41, 51, 235, 12, 51, 23, 512,3 , 512, 313, 512,323, 2351, 123, 34];
+
+const menorValor = valores.reduce((valorAcc, valorAtual) => valorAtual < valorAcc ? valorAcc = valorAtual : valorAcc = valorAcc);
+
+const maiorValor = valores.reduce((valorAcc, valorAtual) => valorAtual > valorAcc ? valorAcc = valorAtual : valorAcc = valorAcc);
+
+console.log(valores);
