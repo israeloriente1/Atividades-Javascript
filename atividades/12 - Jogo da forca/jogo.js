e2f694935e26eef6c510db6a2d29656a93d4dafc
israeloriente1 <israeloriente1@hotmail.com>
2021-11-28 00:49:13 -0300

    Atualização de dados
    - Lista de dados do jogo da forca atualizado
    - Melhoria na organização do algoritmo
    - Ajustes no CSS

--- atividades/12 - Jogo da forca/jogo.js
+++ atividades/12 - Jogo da forca/jogo.js
@@ -3,10 +3,38 @@
    */
 
 listaPalavras = [
-    {dica: "Profissão", palavra: "MOTORISTA"},
-    {dica: "Profissão", palavra: "PROGRAMADOR"},
-    {dica: "Profissão", palavra: "DENTISTA"},
-    {dica: "Profissão", palavra: "ELETRICISTA"},
+    {dica: 'Profissão', palavra: 'ADMINISTRADOR'},
+    {dica: 'Profissão', palavra: 'ADVOGADO'},
+    {dica: 'Profissão', palavra: 'AERONAUTA'},
+    {dica: 'Profissão', palavra: 'ASSISTENTE SOCIAL'},
+    {dica: 'Profissão', palavra: 'ATLETA PROFISSIONAL DE FUTEBOL'},
+    {dica: 'Profissão', palavra: 'BOMBEIRO CIVIL'},
+    {dica: 'Profissão', palavra: 'CONTABILISTA'},
+    {dica: 'Profissão', palavra: 'CORRETOR DE SEGUROS'},
+    {dica: 'Profissão', palavra: 'DESPACHANTE ADUANEIRO'},
+    {dica: 'Profissão', palavra: 'ECONOMISTA'},
+    {dica: 'Profissão', palavra: 'ENFERMAGEM'},
+    {dica: 'Profissão',palavra: 'FISIOTERAPEUTA E TERAPEUTA OCUPACIONAL'},
+    {dica: 'Profissão', palavra: 'GARIMPEIRO'},
+    {dica: 'Profissão', palavra: 'JORNALISTA'},
+    {dica: 'Profissão', palavra: 'LEILOEIRO'},
+    {dica: 'Profissão', palavra: 'LEILOEIRO RURAL'},
+    {dica: 'Profissão', palavra: 'MASSAGISTA'},
+    {dica: 'Profissão', palavra: 'MOTOTAXISTA E MOTOBOY'},
+    {dica: 'Profissão', palavra: 'NUTRICIONISTA'},
+    {dica: 'Profissão', palavra: 'ODONTOLOGIA'},
+    {dica: 'Profissão', palavra: 'ORIENTADOR EDUCACIONAL'},
+    {dica: 'Profissão', palavra: 'PESCADOR PROFISSIONAL'},
+    {dica: 'Profissão', palavra: 'PSICOLOGIA'},
+    {dica: 'Profissão', palavra: 'RADIALISTA'},
+    {dica: 'Profissão', palavra: 'REPENTISTA'},
+    {dica: 'Profissão', palavra: 'SOMMELIER'},
+    {dica: 'Profissão', palavra: 'TAXISTA'},
+    {dica: 'Profissão', palavra: 'ZOOTECNISTA'},
+    {dica: 'Profissão', palavra: 'Taxista'},
+    {dica: "Profissão", palavra: "GEOLOGIA"},
+    {dica: "Profissão", palavra: "CIENTISTA"},
+    {dica: "Profissão", palavra: "METEOROLIGIA"},
     {dica: "País", palavra: "BRASIL"},
     {dica: "País", palavra: "REINO UNIDO"},
     {dica: "País", palavra: "ESTADOS UNIDOS"},
@@ -35,9 +63,11 @@ var linhas; // Irá receber as letras da variável (palavra) como "_".
 sortear(); // Irá guardar cada letra da variável palavra dentro de (palavra).
 
 function sortear(){ // Irá gerar um valor random entre 0 e 2, que será usado para filtrar a categoria da variável (listaPalavras) de acordo com o valor que tiver sido gerado (como está no switch(randomValor))
+    divDica.style.marginTop = "-40px";
     let randomValor = parseInt(Math.random() * 3);
     categoria;
     let palavraObjeto;
+
     switch(randomValor){
         case 0:
             categoria = "Profissão";
@@ -48,14 +78,18 @@ function sortear(){ // Irá gerar um valor random entre 0 e 2, que será usado p
         default:
             categoria = "Objeto";
     }
+
     divDica.innerHTML = `DICA: <br> ${categoria.toUpperCase()}`;
     palavraObjeto = listaPalavras.filter(p => p.dica == `${categoria}`); // Irá receber a lista de objetos contendo (Categoria e palavra) da categoria que tiver sido gerado
     let palavraLength = palavraObjeto.length - 1;
+
     randomValor = parseInt(Math.random() * palavraLength); // Irá receber um valor random entre 0 e o total de indices com a categoria que foi gerado na variável (categoria), esse indice que for gerado será usado para pegar o valor de (palavra).
     palavra = palavraObjeto[randomValor].palavra;
+
     if (palavra == palavraAnterior){ // Caso a palavra gerada seja igual a anterior
         sortear();
     }
+
     linhas = palavra.replace(/[a-z]/ig, "_");
     palavraConv = linhas.replace(" ", "<br>").replace(/[_]/ig, "_ ");
     divPalavra.innerHTML = palavraConv;
@@ -70,12 +104,13 @@ function imprimirLinhaAtual(){ // Apaga o valor atual e imprimi as letras que fo
 function verificar(letra, posicao){ // Irá analisar a letra e a posição informado, o valor do parâmetro (posicao) será usado na variável (spanLetra) pagar pegar o id com o valor do parâmetro (posicao) 
     letra = letra.toUpperCase()
     spanLetra = document.getElementById(`${posicao}`);
+
     if (chances >= 1 && isComplete == false){
         if (spanLetra.hasAttribute("class", "letra")){
             spanLetra.removeAttribute("class", "letra"); // Mudará o estilo da letra selecionada, o que deixará não acessível até o próximo jogo
             palavra.indexOf(letra) > -1 ? isInclude = true : isInclude = false; // Irá verificar se a letra selecionada exista na palavra atual
             if (isInclude == false){ // Caso a letra NÃO exista na palavra, será diminuído o total de chances para jogar novamente.
-                chances--;
+                chances--;                
                 if (chances > 2){
                     divChance.innerHTML = `CHANCES <br>${chances}`;
                 }else{
@@ -88,6 +123,7 @@ function verificar(letra, posicao){ // Irá analisar a letra e a posição infor
                         divDica.innerHTML = `PALAVRA: <br> ${palavra}`
                     }
                 }
+
             }else if (isInclude == true){ // Os índices do vetor (letras) que tiverem a letra selecionada será adicionado no mesmo índice do vetor (linhas).
                 for (let l in palavra){
                     if (palavra.charAt(l) == letra){
@@ -98,13 +134,16 @@ function verificar(letra, posicao){ // Irá analisar a letra e a posição infor
                         }
                     }
                 }
+
                 linhas.indexOf("_") > -1 ? isComplete = false : isComplete = true;
+
                 if (isComplete == true){
                     botao.style.display = "block";
                     if (divChance.hasAttribute("class", "red")){
                         divChance.removeAttribute("class", "red");
                     }
-                    divChance.innerHTML = "CONCLUÍDO"
+                    divDica.style.marginTop = "-20px";
+                    divChance.innerHTML = "CONCLUÍDO <br>"
                 }
                 imprimirLinhaAtual(); // Irá atualizar o status da divLinhas, onde contém as linhas que recebem as letras conforme for selecionando e completando a palavra.
             }
@@ -118,9 +157,11 @@ function novoJogo(){ // Irá criar um novo jogo, deixando todos os botões acess
         spanLetra = document.getElementById(`${n}`);
         spanLetra.setAttribute("class", "letra");
     }
+
     palavraAnterior = palavra;
     chances = 6;
     divChance.innerHTML = `CHANCES <br>${chances}`;
+
     if (divChance.hasAttribute("class", "red")){
         divChance.removeAttribute("Class", "red");
     }
